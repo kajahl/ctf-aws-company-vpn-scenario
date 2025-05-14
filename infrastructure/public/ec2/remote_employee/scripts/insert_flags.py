2df17b326a0f3619f7d69a0be215f965132f82b4
kajahl <kacperjahl37@gmail.com>
2025-05-14 16:00:49 +0200

    add example flag

--- infrastructure/public/ec2/remote_employee/scripts/insert_flags.py
+++ infrastructure/public/ec2/remote_employee/scripts/insert_flags.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import base64
 
 print('Wykonywanie skryptu Python dla instancji EC2: remote_employee')
 module_relative_path = "../infrastructure/public/ec2/remote_employee"
@@ -10,7 +11,7 @@ scripts_path = os.path.join(base_path, "scripts")
 print(scripts_path)
 sys.path.append(scripts_path)
 
-from flags import insert_flag_to_file, replace_placeholder_in_file # type: ignore
+from flags import insert_flag_to_file, replace_placeholder_in_file, get_flag # type: ignore
 
 # Ścieżki do plików (relatywne względem lokalizacji skryptu)
 relative_path_init = os.path.join(base_path, module_relative_path, "init.sh")
@@ -37,10 +38,20 @@ def get_file_path(relative_path: str) -> str:
     """
     return os.path.abspath(os.path.join(relative_files_path, relative_path))
 
+# Customowe base64 dla 19
+flaga_19 = "19"
+filepath_19 = get_file_path("darmowe.txt")
+print(f"Wstawianie flagi ID={flaga_19} do pliku: {filepath_19}")
+
+placeholder, replacement_value = get_flag(flaga_19)
+base64_flaga_19 = base64.b64encode(replacement_value.encode('utf-8')).decode('utf-8')
+replace_placeholder_in_file(filepath_19, placeholder + '_BASE64', base64_flaga_19)
+
 # Lista flag związanych z EC2:Company_SOC
 flags = [
     {"id": "17", "file": "moje/przepisy/bigos.txt"},
-    {"id": "18", "file": "firmowe/faktura.txt"}
+    {"id": "18", "file": "firmowe/faktura.txt"},
+    {"id": "19", "file": "darmowe.txt"},
 ]
 
 # Iteracja po flagach i wstawianie ich do odpowiednich plików
@@ -50,3 +61,4 @@ for flag in flags:
 
     print(f"Wstawianie flagi ID={flag_id} do pliku: {file_path}")
     insert_flag_to_file(flag_id, file_path)
+

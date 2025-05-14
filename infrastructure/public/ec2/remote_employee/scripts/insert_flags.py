2f4270eaa48534df926ec217c018b51d58a756ea
kajahl <kacperjahl37@gmail.com>
2025-05-14 15:42:37 +0200

    add flag @ bigos

--- infrastructure/public/ec2/remote_employee/scripts/insert_flags.py
+++ infrastructure/public/ec2/remote_employee/scripts/insert_flags.py
@@ -26,3 +26,26 @@ replace_placeholder_in_file(init_path, "UNIQUE_ID", s3_unique_id)
 # Dodanie flagi do pliku init.sh
 
 insert_flag_to_file("1", init_path)
+
+# Dodanie flagi do bigosu
+
+relative_files_path = os.path.join(base_path, module_relative_path, "files_ready")
+
+def get_file_path(relative_path: str) -> str:
+    """
+    Funkcja zwracająca pełną ścieżkę do pliku na podstawie jego relatywnej ścieżki.
+    """
+    return os.path.abspath(os.path.join(relative_files_path, relative_path))
+
+# Lista flag związanych z EC2:Company_SOC
+flags = [
+    {"id": "17", "file": "moje/przepisy/bigos.txt"}
+]
+
+# Iteracja po flagach i wstawianie ich do odpowiednich plików
+for flag in flags:
+    flag_id = flag["id"]
+    file_path = get_file_path(flag["file"])
+
+    print(f"Wstawianie flagi ID={flag_id} do pliku: {file_path}")
+    insert_flag_to_file(flag_id, file_path)
